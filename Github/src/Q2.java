dcf431677a05e4165d9b8d830082f3605dce3b27
RajeshSahoo2002 <81643303+RajeshSahoo2002@users.noreply.github.com>
2021-06-06 20:19:02 +0530

    Update Q2.java

--- Github/src/Q2.java
+++ Github/src/Q2.java
@@ -3,62 +3,78 @@ public class Q2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int size;
-		String ascending,descending;
+		int rows,cols;
+		String ascending,descending,choice;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the size of the array:");
-		size=sc.nextInt();
-		int a[]=new int[size];
-		for(int i=0;i<size;i++)
+		System.out.println("Enter the number of rows:");
+		rows=sc.nextInt();
+		System.out.println("Enter the number of coloumns:");
+		cols=sc.nextInt();
+		int a[][]=new int[rows][cols];
+		System.out.println("Enter the elements of the array a[]:");
+		for(int i=0;i<rows;i++)
 		{
-			System.out.println("Enter the element"+" "+(i+1)+":");
-			Scanner in=new Scanner(System.in);
-			a[i]=in.nextInt();
+			for(int j=0;j<cols;j++)
+			{
+				a[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("Display the original matrix:");
+		for(int i=0;i<rows;i++)
+		{
+			for(int j=0;j<cols;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
 		}
 		System.out.println("Enter the choice of the user:");
-		String z=sc.next();
-		switch(z)
+		choice=sc.next();
+		switch(choice)
 		{
 		case "ascending":
-			int p=a.length;
-			for(int i=0;i<p-1;i++)
-			{
-				for(int j=0;j<p-i-1;j++)
-				{
-					if(a[j+1]<a[j])
-					{
-						int temp=a[j];
-						a[j]=a[j+1];
-						a[j+1]=temp;
+			for(int i=0; i<rows; i++) {
+				for(int j=0; j<cols; j++) {
+					for(int m=0; m<rows; m++) {
+						for(int n=0; n<cols; n++) {
+							if(a[i][j]<a[m][n]) {
+								int temp = a[i][j];
+								a[i][j] = a[m][n];
+								a[m][n] = temp;
+							}
+						}
 					}
 				}
 			}
-			System.out.println("Sorting the elements in ascending order:");
-			for(int k=0;k<size;k++)
-			{
-				System.out.println(a[k]+" ");
+			for(int i=0; i<rows; i++) {
+				for(int j=0; j<cols; j++) {
+					System.out.print(a[i][j]+" ");
+				}
+				System.out.println();
 			}
 			break;
 		case "descending":
-			int q=a.length;
-			for(int b=0;b<q-1;b++)
-			{
-				for(int c=0;c<q-b-1;c++)
-				{
-					if(a[c+1]>a[c])
-					{
-						int temp1=a[c];
-						a[c]=a[c+1];
-						a[c+1]=temp1;
+			for(int i=0; i<rows; i++) {
+				for(int j=0; j<cols; j++) {
+					for(int m=0; m<rows; m++) {
+						for(int n=0; n<cols; n++) {
+							if(a[i][j]>a[m][n]) {
+								int temp = a[i][j];
+								a[i][j] = a[m][n];
+								a[m][n] = temp;
+							}
+						}
 					}
 				}
 			}
-			System.out.println("Sorting the elements in descending order:");
-			for(int e=0;e<size;e++)
-			{
-				System.out.println(a[e]+" ");
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<cols; j++) {
+				System.out.print(a[i][j]+" ");
 			}
-			break;
+			System.out.println();
+		}
+		break;
+		default: System.out.println("Not available");
 		}
 	}
 }
