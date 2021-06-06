f73cfe688256309a14f855676bb2f864dee70cc3
RajeshSahoo2002 <81643303+RajeshSahoo2002@users.noreply.github.com>
2021-06-06 18:14:13 +0530

    Update Q7.java

--- Github/src/Q7.java
+++ Github/src/Q7.java
@@ -6,5 +6,8 @@ public class Q7 {
 		int i,n,d=5;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the value of n for which i is asked:");
+		n=sc.nextInt();
+		i=((n+7)/5)-1;
+		System.out.println("Value of i for which n is " +i);
 	}
 }
